76188881bd2fdcfd5d32ade1db9d6b9158c9c7c0
nakulvaghela11 <nakulvaghela11@gmail.com>
2025-08-28 10:24:25 +0530

    Lab12 ListView and ListView.builder demo

---
+++ lib/Lab12/Practical1.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class ContactListPage extends StatelessWidget {
+  ContactListPage({super.key});
+
+  final List<Map<String, String>> contacts = [
+    {'name': 'Dhoni', 'initial': 'D'},
+    {'name': 'Virat', 'initial': 'V'},
+    {'name': 'Rohit', 'initial': 'R'},
+    {'name': 'Jadeja', 'initial': 'J'},
+    {'name': 'Bumrah', 'initial': 'B'},
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Contact List')),
+      body: ListView.builder(
+        itemCount: contacts.length,
+        itemBuilder: (context, index) {
+          var contact = contacts[index];
+          return Card(
+            margin: EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+            child: ListTile(
+              leading: CircleAvatar(
+                child: Text(contact["initial"]!),
+
+              ),
+              title: Text(contact['name']!),
+              trailing: Icon(Icons.call),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}

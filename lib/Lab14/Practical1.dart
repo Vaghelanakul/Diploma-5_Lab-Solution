d439896072e7d20812edcd90f439a7878a487b91
nakulvaghela11 <nakulvaghela11@gmail.com>
2025-08-28 10:54:39 +0530

    Lab14 PageView demo

---
+++ lib/Lab14/Practical1.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class PageDemo extends StatefulWidget {
+  const PageDemo({super.key});
+
+  @override
+  State<PageDemo> createState() => _PageDemoState();
+}
+
+class _PageDemoState extends State<PageDemo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Page View demo"),
+      ),
+      body: PageView(
+        scrollDirection: Axis.vertical,
+        children: [
+          buildPage(title: "page1", color: Colors.green),
+          buildPage(title: "page2", color: Colors.blue),
+          buildPage(title: "page3", color: Colors.yellow),
+        ],
+      ),
+    );
+  }
+
+  Widget buildPage({required String title, required Color color}) {
+    return Padding(
+      padding: const EdgeInsets.all(20),
+      child: Card(
+        elevation: 10,
+        color: color,
+        child: Center(
+          child: Text(title,style: TextStyle(fontSize: 30),),
+        ),
+      ),
+    );
+  }
+}
